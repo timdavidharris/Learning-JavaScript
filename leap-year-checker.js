db74440625eb9afa3bb348c80dc96c5ac3e29a0a
Tim David Harris <timdavidharris@gmail.com>
2021-06-03 20:04:00 -0600

    Switching the code from asking for a prompt to acting as a function to check for the leap year

--- leap-year-checker.js
+++ leap-year-checker.js
@@ -1,12 +1,17 @@
-let year = prompt("What year are you trying to figure out if it is a leap year?\n");
-
-if (year % 4 === 0)
-  if (year % 100 === 0)
-    if (year % 400 === 0)
-      console.log("The year " + year + " is a leap year."); 
+function isLeap(year) {
+  if (year % 4 === 0)
+    if (year % 100 === 0)
+      if (year % 400 === 0)
+        console.log("The year " + year + " is a leap year.");
+      else 
+        console.log("The year " + year + " is not a leap year.");
     else 
-      console.log("The year " + year + " is not a leap year.");
+      console.log("The year " + year + " is a leap year.");
   else 
-    console.log("The year " + year + " is a leap year.");
-else 
-  console.log("The year " + year + " is not a leap year.");
+    console.log("The year " + year + " is not a leap year.");
+  }
+  
+  isLeap(2000);
+  isLeap(1948);
+  isLeap(1905);
+  isLeap(100);  
