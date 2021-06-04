6c4dab6ba11fb5aedf5b07e4bb98175474002ddf
Tim David Harris <timdavidharris@gmail.com>
2021-06-03 19:33:48 -0600

    Removing the function to try and return an intrepretation as I was doind it wrong and the section was only looking for the bmi number

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -1,25 +1,14 @@
 function bmiCalculator(weight, height) {
-    let bmi = Math.round(weight / Math.pow(height, 2));
-    return interpretation;
-  }
-  
-  function interpretation() {
-    if (bmi < 18.5) {
-      console.log("Your BMI is " + bmi + ", so you are underweight.");
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-      console.log("Your BMI is " + bmi + ", so you have a normal weight.");
-    } else if (bmi === 24.9) {
-      console.log("Your BMI is " + bmi + ", so you are overweight.");
-    }
-  }
-  
-  let bmi = bmiCalculator(65, 1.8);
-  console.log(bmi);
-  console.log(interpretation);
-  
-  
-  /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
-  
-  bmi should equal 20 when it's rounded to the nearest whole number.
-  
-  */
+  var bmi = Math.round(weight / Math.pow(height, 2));
+  return bmi;
+}
+
+var bmi = bmiCalculator(65, 1.8);
+console.log(bmi);
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
