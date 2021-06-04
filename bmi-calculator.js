d19b4d4964e687eedd73cb3bad464168bff52d49
Tim David Harris <timdavidharris@gmail.com>
2021-06-03 19:56:05 -0600

    The solution to the advanced bmi calculator, referenced another's GitHub to understand the function set up

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -1,10 +1,19 @@
 function bmiCalculator(weight, height) {
-  var bmi = Math.round(weight / Math.pow(height, 2));
-  return bmi;
+  var bmi = Math.round(weight / (height * height));
+  var interpretation = "";
+  if (bmi < 18.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+  } else if (bmi > 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  } else {
+    interpretation = "Invalid";
+  }
+  return interpretation;
 }
 
-var bmi = bmiCalculator(65, 1.8);
-console.log(bmi);
+var bmi = bmiCalculator(60, 2);
 
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
