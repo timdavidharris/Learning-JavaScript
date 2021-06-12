18d481f3aae0164ee1b661987ebc10701dddfce9
Tim David Harris <timdavidharris@gmail.com>
2021-06-11 20:13:19 -0600

    My code so far: I have not figured out how to get my for loop to work

--- fibonacci-sequence-loop.js
+++ fibonacci-sequence-loop.js
@@ -1,5 +1,24 @@
-
 function fibonacciGenerator(n) {
-    for (var i = 0; i < 10; i ++);
+    var output = [];
+    if (n === 1) {
+        output.push(0);
+    } else if (n === 2) {
+        output.push(0, 1)
+    } else {
+        var output = [0, 1, 1];
+        var lastArrayNumber = output.slice(-1).pop();
+        var secondToLastArrayNumber = output.slice(-2).pop();
+        var newNumber = lastArrayNumber + secondToLastArrayNumber;
+        for (n = 3; n == output.length; output.push(newNumber)) {
+            console.log(output);
+        }
+      }
+    console.log(output);
+    console.log(lastArrayNumber);
+    console.log(secondToLastArrayNumber);
+    console.log(newNumber);
+    console.log(output.length)
 
 }
+
+fibonacciGenerator(10);
