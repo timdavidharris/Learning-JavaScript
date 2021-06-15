91e8ab97f5bbec4a75bb8857fac38050e3951883
Tim David Harris <timdavidharris@gmail.com>
2021-06-14 20:30:35 -0600

    Adding comments to my code

--- fibonacci-sequence-loop.js
+++ fibonacci-sequence-loop.js
@@ -6,8 +6,9 @@ function fibonacciGenerator(n) {
         output.push(0, 1)
     } else {
         output = [0, 1];
-
+        // the for loop has to all refer to the same variable
         for (var i = 2; i < n; i++) {
+            // output[output.length - i] allows us to select the last or second to last item from an array
             output.push(output[output.length - 2] + output[output.length - 1]);
         }
       }
@@ -16,6 +17,7 @@ function fibonacciGenerator(n) {
 
 }
 
+// I am running 3 different numbers through the function to test multiple items at one time
 fibonacciGenerator(1);
 fibonacciGenerator(10);
 fibonacciGenerator(20);
